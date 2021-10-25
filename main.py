6bed85350863900c1a14a34419bce181c29835dc
tja15 <t-ja15@wp.pl>
2021-10-25 22:47:19 +0200

    almost ready

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from selenium import webdriver
-from bs4 import BeautifulSoup as bs
 import json
+
+from bs4 import BeautifulSoup as bs
+from selenium import webdriver
 
 from course import Course
 
@@ -23,7 +24,8 @@ def get_all_links(category_links, driver):
                 res.append(x['href'])
     return res
 
-def create_course_obj(link, driver):
+
+def create_course_obj(link, driver, id):
     driver.get(link)
     html = driver.page_source
     soup = bs(html, "html.parser")
@@ -31,14 +33,16 @@ def create_course_obj(link, driver):
     title = entry_summary.find('h1').text
     if title == "Karta podarunkowa":
         return None
-    price = entry_summary.find('bdi').text + " zł"
+    price = entry_summary.find('bdi').text
 
     tmp = entry_summary.find('div', {'class': "woocommerce-product-details__short-description"}).find('p')
     if tmp is not None:
         short_desc = tmp.text
     else:
         short_desc = entry_summary.find('div', {'class': "error-span misspelling tooltipstered"}).text
-        
+
+    category = soup.find('span', {'class': "posted_in"}).find('a').text
+
     img_container = soup.find('ol', {'class': "flex-control-nav flex-control-thumbs"})
     imgs = []
     n_flag = False
@@ -52,7 +56,11 @@ def create_course_obj(link, driver):
     for i in imgs:
         if not n_flag:
             x = i.find('img')
-            sources.append(x['src'])
+            p = x['src']
+            ext = p[-4:]
+            p = p[:-12]
+            p += ext
+            sources.append(p)
         else:
             sources.append(i[0])
     description_container = soup.find('div', {'id': "tab-description"})
@@ -60,14 +68,17 @@ def create_course_obj(link, driver):
     if description_container is not None:
         ps = description_container.find_all('p')
         for p in ps:
-            content = p.text
+            content = '<p>' + p.text + '</p>'
             text += (content + '\n')
 
     # print(text)
-    return Course(title, short_desc, text, price, sources, link)
+    ret = Course(title, short_desc, text, price, sources, link, id)
+    ret.set_category(category)
+    return ret
 
-# PATH = "C:\Program Files (x86)\chromedriver.exe"
-PATH = "/home/user/scrap/scraping/chromedriver"
+
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+# PATH = "/home/user/scrap/scraping/chromedriver"
 driver = webdriver.Chrome(PATH)
 courses = []
 
@@ -81,10 +92,10 @@ all = get_all_links(links, driver)
 # print(all)
 
 # create_course_obj(all[0], driver)
-it = 0
+it = 1
 for link in all:
     print(it)
-    x = create_course_obj(link, driver)
+    x = create_course_obj(link, driver, it)
     if x is not None:
         courses.append(x)
     it += 1
@@ -97,3 +108,4 @@ dumps = json.dumps(jsons)
 with open("objects.txt", 'w+') as file:
     file.write(dumps)
     file.close()
+driver.close()
